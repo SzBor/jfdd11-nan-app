5a4c8e10a9043e903d4b8e9b95a2d2593b4610c2
daniel-mikula <daniel.mikula11@gmail.com>
2019-02-22 14:59:21 +0100

    HomeView component folder structure created

---
+++ src/components/HomeView/HomeView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './HomeView.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="HomeView">
+        <header className="">
+          <p>Learn React</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default HomeView;
