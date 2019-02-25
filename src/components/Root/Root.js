c678bf858104cb174dd3de35a5a49ff0a2e2583e
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2019-02-25 16:24:33 +0100

    data fetching functions declared in services, conection to firebase in firebasesetup.js

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from '../App';
-import MainMenu from '../MainMenu';
 import TrackPanel from '../TrackPanel';
 import Dashboard from '../Dashboard';
 
