e2134db2a6c0a77b3dba8dd871d0afecc389fe90
Szymon Borucki <szymborucki@gmail.com>
2019-02-25 17:00:13 +0100

    signup form skeleton added

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react'
-
-import './SignUp.css'
+import React, { Component } from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+import "./SignUp.css";
 
 class SignUp extends Component {
   render() {
     return (
-      <div className="SignUp">SignUp</div>
-    )
+      <div className="SignUp">
+        <Form>
+          <Form.Group>
+            <Form.Input label="First name" placeholder="First Name" width={6} />
+            <Form.Input
+              label="Middle Name"
+              placeholder="Middle Name"
+              width={4}
+            />
+            <Form.Input label="Last Name" placeholder="Last Name" width={6} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Input placeholder="2 Wide" width={2} />
+            <Form.Input placeholder="12 Wide" width={12} />
+            <Form.Input placeholder="2 Wide" width={2} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Input placeholder="8 Wide" width={8} />
+            <Form.Input placeholder="6 Wide" width={6} />
+            <Form.Input placeholder="2 Wide" width={2} />
+          </Form.Group>
+          <Form.Checkbox label='I agree to the Terms and Conditions' />
+          <Button>Submit</Button>
+        </Form>
+      </div>
+    );
   }
 }
 
-export default SignUp
+export default SignUp;
