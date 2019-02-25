b005a15c797758c23758f7aff1763cea10805b39
Szymon Borucki <szymborucki@gmail.com>
2019-02-25 16:21:42 +0100

    main manu added

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,28 +1,65 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+import image from "./trackenLogo.svg"
+
+import "./mainMenu.css";
 
 class MainMenu extends Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
+    const { activeItem } = this.state;
+
     return (
-      <div className="MainMenu">
-        <h2>Main menu</h2>
-        <ul>
-          <li>
-            <NavLink exact to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/app">Default App</NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard">Dashboard</NavLink>
-          </li>
-          <li>
-            <NavLink to="/trackpanel">Track panel</NavLink>
-          </li>
-        </ul>
-      </div>
+      <Menu stackable>
+        <Menu.Item>
+          <img src={image} />
+        </Menu.Item>
+
+        <Menu.Item
+          name="Home"
+          active={activeItem === "Home"}
+          onClick={this.handleItemClick}
+          to="/home"
+          as={NavLink}
+        >
+          Home
+        </Menu.Item>
+
+        <Menu.Item
+          name="app"
+          active={activeItem === "app"}
+          onClick={this.handleItemClick}
+          to="/App"
+          as={NavLink}
+        >
+          App
+        </Menu.Item>
+
+        <Menu.Item
+          name="dashboard"
+          active={activeItem === "dashboard"}
+          onClick={this.handleItemClick}
+          to="/dashboard"
+          as={NavLink}
+        >
+          Dashboard
+        </Menu.Item>
+
+        
+        <Menu.Item
+          name="track-panel"
+          active={activeItem === "track-panel"}
+          onClick={this.handleItemClick}
+          to="/trackpanel"
+          as={NavLink}
+        >
+          Track Panel
+        </Menu.Item>
+      </Menu>
     );
   }
 }
