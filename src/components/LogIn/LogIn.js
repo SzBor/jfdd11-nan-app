d121c5ed42f0135aa335e88139a4e3882d8d2c20
daniel-mikula <daniel.mikula11@gmail.com>
2019-02-25 17:00:48 +0100

    login routing

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react'
-import { Button, Form } from 'semantic-ui-react'
-import './LogIn.css'
+import React, { Component } from "react";
+import { Button, Form } from "semantic-ui-react";
+import "./LogIn.css";
 
 class LogIn extends Component {
+  state = {};
+
   render() {
     return (
-      <div className="LogIn">
-      {const FormExampleUnstackable = () => (
-  <Form unstackable>
-    <Form.Group widths={2}>
-      <Form.Input label='Email' placeholder='email' />
-      <Form.Input label='Password' placeholder='password' />
-    </Form.Group>
-    <Button type='login'>Log In</Button>
-      </Form>)}
+      <div>
+        <Form unstackable>
+          <Form.Group widths={2}>
+            <Form.Input label="Email" placeholder="email" />
+            <Form.Input label="Password" placeholder="password" />
+          </Form.Group>
+          <Button type="login">Log In</Button>
+        </Form>
       </div>
-    )
+    );
   }
 }
-
-export default LogIn
-
+export default LogIn;
