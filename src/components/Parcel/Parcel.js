bc27a30472815fca130bbd964bf49e17d7a0c27c
Vladyslav Ganushkevych <ganushkevychv@gmail.com>
2019-02-25 17:00:27 +0100

    status and details fetched

--- src/components/Parcel/Parcel.js
+++ src/components/Parcel/Parcel.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Leaflet from "leaflet";
 
 import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
-import { red } from "ansi-colors";
+import { getPackagesPromise } from "../../services";
 Leaflet.Icon.Default.imagePath =
   "//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/";
 
@@ -12,8 +12,14 @@ class Parcel extends Component {
     lng: 18.5694926,
     zoom: 15,
     latDelivered: 54.352,
-    lngDelivered: 18.6466
+    lngDelivered: 18.6466,
+      packages: [],
+      customers: ""
   };
+  getLocation
+ 
+  syncPackages = () =>
+    getPackagesPromise().then(packages => this.setState({ packages }));
 
   getIcon = () => {
     const icon = new Leaflet.Icon({
@@ -23,7 +29,10 @@ class Parcel extends Component {
     });
     return icon;
   };
+  componentDidMount() {
+    this.syncPackages();
 
+  }
   render() {
     const position = [this.state.lat, this.state.lng];
     const positionDelivered = [
@@ -46,13 +55,30 @@ class Parcel extends Component {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
           />
-          <Marker icon={this.getIcon()} style={{ background: red }} position={position}>
+          <Marker icon={this.getIcon()}  position={position}>
             <Popup>Sending address</Popup>
           </Marker>
           <Marker position={positionDelivered}>
             <Popup>Delivery address</Popup>
           </Marker>
         </LeafletMap>
+        <table className="ui celled table">
+          <thead>
+            <tr>
+              <th>Status</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.packages.map(pack => (
+              <tr key={pack.id}>
+                <td>{pack.status}</td>
+                <td>{pack.delivery.city}</td>
+                <td />
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <button>Parcel delivered</button>
       </div>
     );
