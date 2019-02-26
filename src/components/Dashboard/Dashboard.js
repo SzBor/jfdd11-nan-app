18bf2722808d8734f803e0319d4757088c985940
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2019-02-26 15:26:18 +0100

    added display packages with sorting based on a date send

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import MainMenu from "../MainMenu";
+import {Link} from 'react-router-dom'
+import moment from 'moment'
 import "./Dashboard.css";
 import { getCustomersPromise, getPackagesPromise } from "../../services";
 
@@ -30,23 +32,23 @@ class Dashboard extends Component {
         <table className="ui celled table">
           <thead>
             <tr>
+              <th>Data send</th>
               <th>Status</th>
-              <th>ClientID</th>
               <th>Delivery Name</th>
-              <th>Delivery city</th>
-              <th>Delivery street</th>
+              <th>Delivery address</th>
               <th>Details</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.packages.map(pack => (
+            {this.state.packages.slice()
+            .sort((a, b) => moment(a.date_send).isAfter(b.date_send) ? -1 : 1)
+            .map(pack => (
               <tr key={pack.id}>
-                <td>{pack.status}</td>
-                <td>{pack.client_id}</td>
-                <td>{pack.delivery.city}</td>
-                <td>{pack.dimensions.width}</td>
-                <td />
-                <td>x</td>
+                <td>{pack.date_send}</td>
+                <td style={{color:pack.status==='received' ? '#006622' : (pack.status==='send' ? '#0099ff' : '#e68a00')}}>{pack.status}</td>
+                <td>{pack.delivery.name}</td>
+                <td>{pack.delivery.city}, {pack.delivery.address}</td>
+                <td><Link to={`/dashboard/${pack.id}`}><button>Details</button></Link></td>
               </tr>
             ))}
           </tbody>
