50c4c71bce2547ca32721cfa2cda904d8f4f3753
Szymon Borucki <szymborucki@gmail.com>
2019-02-26 15:00:02 +0100

    signup page linked to signup button

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -8,7 +8,7 @@ class SignUp extends Component {
       <div className="SignUp">
         <Form>
           <Form.Group>
-            <Form.Input label="First name" placeholder="First Name" width={6} />
+            <Form.Input label="First name" placeholder="First Name" width={4} />
             <Form.Input
               label="Middle Name"
               placeholder="Middle Name"
@@ -17,15 +17,11 @@ class SignUp extends Component {
             <Form.Input label="Last Name" placeholder="Last Name" width={6} />
           </Form.Group>
           <Form.Group>
-            <Form.Input placeholder="2 Wide" width={2} />
-            <Form.Input placeholder="12 Wide" width={12} />
-            <Form.Input placeholder="2 Wide" width={2} />
+            <Form.Input label = "Postal code" placeholder="Postal code" width={2} />
+            <Form.Input label="Address" placeholder="Address" width={10} />
+           
           </Form.Group>
-          <Form.Group>
-            <Form.Input placeholder="8 Wide" width={8} />
-            <Form.Input placeholder="6 Wide" width={6} />
-            <Form.Input placeholder="2 Wide" width={2} />
-          </Form.Group>
+          
           <Form.Checkbox label='I agree to the Terms and Conditions' />
           <Button>Submit</Button>
         </Form>
