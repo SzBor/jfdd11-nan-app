b64793e283bcb15c65aa1c06fa3d566feda96e49
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2019-02-28 14:21:58 +0100

    little changes in details variable names of packages

--- src/components/Parcel/Parcel.js
+++ src/components/Parcel/Parcel.js
@@ -93,8 +93,9 @@ class Parcel extends Component {
           <tbody>
             <tr>
               <td>{parcel.date_send}</td>
-            <td>{parcel.delivery.address}</td>
-              <td>{parcel.delivery.city}</td>
+            <td>`{parcel.delivery.city} {parcel.delivery.postalcode},
+            {parcel.delivery.street} {parcel.delivery.number}`</td>
+              <td>{parcel.courier_id}</td>
               <td>{parcel.date_delivery}</td>
               <td>depth(mm): {parcel.dimensions.depth}<br/>
              height(mm): {parcel.dimensions.height}<br/>
