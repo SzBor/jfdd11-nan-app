f4e3ca407cf90eb16047ee3023fc801260425b28
daniel-mikula <daniel.mikula11@gmail.com>
2019-02-28 16:42:02 +0100

    added icons in login form

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 import { Button, Form } from "semantic-ui-react";
-import { withRouter } from 'react-router-dom'
+import { withRouter } from "react-router-dom";
 
-import firebase from 'firebase';
+import firebase from "firebase";
 
 import "./LogIn.css";
 
 class LogIn extends Component {
   state = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     error: null,
-    success: null,
+    success: null
   };
 
   handleChange = event => {
     this.setState({
-      [event.target.name]: event.target.value,
+      [event.target.name]: event.target.value
     });
   };
 
@@ -27,33 +27,43 @@ class LogIn extends Component {
       .auth()
       .signInWithEmailAndPassword(this.state.email, this.state.password)
       .then(data => {
-        this.props.history.push('/dashboard')
-        this.setState({ error: null, success: 'Sign in successful' })})
+        this.props.history.push("/dashboard");
+        this.setState({ error: null, success: "Sign in successful" });
+      })
       .catch(error => this.setState({ error: error, success: null }));
   };
-
 
   render() {
     return (
       <div className="LogIn">
-      {this.state.error && (
-          <p style={{ color: 'red' }}>{this.state.error.message}</p>
+        {this.state.error && (
+          <p style={{ color: "red" }}>{this.state.error.message}</p>
         )}
         {this.state.success && (
-          <p style={{ color: 'green' }}>{this.state.success}</p>
+          <p style={{ color: "green" }}>{this.state.success}</p>
         )}
-        <Form onSubmit={this.handleSubmit} unstackable>
+        <Form size="large" onSubmit={this.handleSubmit} unstackable>
           <Form.Group widths={1}>
-            <Form.Input 
-            onChange={this.handleChange}
-            name= "email"
-            value={this.state.email}
-            type="email" label="Email" placeholder="email" />
-            <Form.Input 
-            onChange={this.handleChange}
-            name= "password"
-            value={this.state.password}
-            type="password" label="Password" placeholder="password" />
+            <Form.Input
+            icon="user"
+            iconPosition="left"
+              onChange={this.handleChange}
+              name="email"
+              value={this.state.email}
+              type="email"
+              label="Email"
+              placeholder="email"
+            />
+            <Form.Input
+              icon="lock"
+              iconPosition="left"
+              onChange={this.handleChange}
+              name="password"
+              value={this.state.password}
+              type="password"
+              label="Password"
+              placeholder="password"
+            />
           </Form.Group>
           <Button type="submit">Log In</Button>
         </Form>
