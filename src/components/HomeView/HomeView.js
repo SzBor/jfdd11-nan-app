3425540d23a0e1f9b8e450ee02930cc05aae5eb2
daniel-mikula <daniel.mikula11@gmail.com>
2019-03-07 16:53:24 +0100

    fetching packages to homeview state and display parcel details

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -8,7 +8,7 @@ import Auth from "../Auth/Auth";
 import { withAuth } from "../../contexts/AuthContext";
 import firebase from "firebase";
 import image from "../MainMenu/trackenLogo.svg";
-
+import { getPackagesPromise } from "../../services";
 import "./HomeView.css";
 
 class CustomButton extends Component {
@@ -18,12 +18,38 @@ class CustomButton extends Component {
 }
 
 class HomeView extends Component {
-  state = {};
+  state = {
+    parcel: {
+      client_id: "-LZZyhU5d6efiXiWz1B-",
+      date_order: "2018-02-23",
+      date_send: "2018-12-04",
+      dimensions: {
+        depth: "",
+        height: "",
+        weight: "34",
+        width: "24"
+      },
+      id: "-LZoFSPw4Nb7sLajyxjK",
+      latitude: 52.6269375,
+      longitude: 16.4463703,
+      status: "pending"
+    }
+  };
+
+  componentDidMount() {
+    const { parcelId } = this.props.match.params;
+    getPackagesPromise().then(data => {
+      this.setState({
+        parcel: data.find(parcel => parcel.id === "-LZoFSPwNb7sLajyxjK") || null
+      });
+    });
+  }
 
   hideFixedMenu = () => this.setState({ fixed: false });
   showFixedMenu = () => this.setState({ fixed: true });
 
   render() {
+    const { parcel } = this.state;
     return (
       <div className="HomeView">
         <Auth
@@ -75,10 +101,50 @@ class HomeView extends Component {
             <SearchBar />
           </div>
         </div>
-{/*         <div className="homeView-footer">
+        {/*         <div className="homeView-footer">
           Footer
           <div>Contact us</div>
         </div> */}
+        {(parcel === null)? <p style={{textAlign:"center"}}>"Incorrect package number "</p>:<table className="ui celled table">
+          <thead>
+            <tr>
+              <th>Sending Date</th>
+              <th>Status</th>
+              <th>Courier ID</th>
+              <th>Delivery Date</th>
+              <th>Dimensions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{parcel.date_send}</td>
+              <td
+                style={{
+                  color:
+                    parcel.status === "received"
+                      ? "#006622"
+                      : parcel.status === "send"
+                      ? "#0099ff"
+                      : "#e68a00"
+                }}
+              >
+                {parcel.status}
+              </td>
+              <td>{parcel.courier_id}</td>
+              <td>{parcel.date_delivery}</td>
+              <td>
+                depth(mm): {parcel.dimensions.depth}
+                <br />
+                height(mm): {parcel.dimensions.height}
+                <br />
+                width(mm): {parcel.dimensions.width}
+                <br />
+                weight(kg): {parcel.dimensions.weight}
+                <br />
+              </td>
+            </tr>
+          </tbody>
+        </table>}
       </div>
     );
   }
