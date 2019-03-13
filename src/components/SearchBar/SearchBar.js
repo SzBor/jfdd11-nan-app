56cee87e225ad75d663f0c0c6cb6bf0377a557fe
daniel-mikula <daniel.mikula11@gmail.com>
2019-03-13 15:56:32 +0100

    details component removed

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -40,7 +40,6 @@ class SearchBar extends Component {
             </Form.Field>
           </Form>
         </div>
-        <DetailsTable />
         <div id="detailsParcel" >
           {parcel === "" && <p style={{ textAlign: "center" }} />}
           {parcel === null && (
@@ -96,60 +95,6 @@ class SearchBar extends Component {
   }
 }
 
-class DetailsTable extends Component {
-  render() {
-    const { parcel } = this.props;
-    return (
-      <div className="parcel-details">
-        {parcel === "" && <p style={{ textAlign: "center" }} />}
-        {parcel === null && (
-          <p style={{ textAlign: "center" }}>"Incorrect package number "</p>
-        )}
-        {parcel && (
-          <table className="ui celled table">
-            <thead>
-              <tr>
-                <th>Sending Date</th>
-                <th>Status</th>
-                <th>Courier ID</th>
-                <th>Delivery Date</th>
-                <th>Dimensions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{parcel.date_send}</td>
-                <td
-                  style={{
-                    color:
-                      parcel.status === "received"
-                        ? "#006622"
-                        : parcel.status === "send"
-                        ? "#0099ff"
-                        : "#e68a00"
-                  }}
-                >
-                  {parcel.status}
-                </td>
-                <td>{parcel.courier_id}</td>
-                <td>{parcel.date_delivery}</td>
-                <td>
-                  depth(mm): {parcel.dimensions.depth}
-                  <br />
-                  height(mm): {parcel.dimensions.height}
-                  <br />
-                  width(mm): {parcel.dimensions.width}
-                  <br />
-                  weight(kg): {parcel.dimensions.weight}
-                  <br />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        )}
-      </div>
-    );
-  }
-}
+
 
 export default SearchBar;
